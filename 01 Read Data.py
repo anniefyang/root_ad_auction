eec52c19a9449b75f3e36d90c4d6f6ea43feba97
unknown <anniefyang@gmail.com>
2019-05-29 12:15:18 -0400

    first pass at concatenating and downsampling data

---
+++ 01 Read Data.py
@@ -0,0 +1,59 @@
+# Parse Root Ad data
+
+# Import libraries
+import pandas as pd
+from os import listdir
+
+# Directory containing project files
+DIR = r'C:/Users/Annie/Documents/Python Bootcamp Final Project'
+
+# Name of folder in DIR containing csv data
+FOLDER_CSV = 'root_ad_auction_dataset_all_data'
+
+# Get names of all files in directory
+file_names = listdir(DIR + '/' + FOLDER_CSV)
+
+# Columns to drop 
+# month, year, and app_bundle have the same value for all observations
+# creative_size encodes the same information as creative_type, so drop 
+# creative_size
+drop_cols = ['month', 'year', 'app_bundle', 'creative_size']
+
+# Initialize the dataframe with the first file, consisting of just column names
+df = pd.read_csv(DIR + '/' + FOLDER_CSV + '/' + file_names[0])
+
+# Drop unnecessary columns
+df = df.drop(drop_cols, 1)
+
+# Drop the first file name (headers only) from daily_data
+del file_names[0]
+
+# Read the remaining data and append to df
+# This takes about 5 minutes
+for i in file_names:
+    # Read daily csv
+    temp = pd.read_csv(DIR + '/' + FOLDER_CSV + '/' + i)
+    
+    # Drop unnecessary columns
+    temp = temp.drop(drop_cols, 1)
+    
+    # Split daily csv into two dataframes: click and no click
+    # Note: observations for which clicks = 0 & installs = 1 should actually
+    # read clicks = 1 & installs = 1
+    click = temp[(temp.clicks == 1) | (temp.installs == 1)]
+    click['clicks'] = 1.0
+    no_click = temp[temp.clicks == 0]
+    
+    # Downsample no click to be the same size as click
+    # For reproducibility, set random_state to 0
+    no_click_sample = no_click.sample(n = click.shape[0], random_state = 0)
+    df = pd.concat([df, click, no_click_sample])
+    
+    # Visually track progress by printing the file name
+    print(i)
+
+# Save dataset as pickle
+df.to_pickle(DIR + '/raw_df.pickle')
+
+# Test reading data back in
+test = pd.read_pickle(DIR + '/raw_df.pickle')
